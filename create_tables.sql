1b239ce85ad5cabdf0e75a4d1831db3572681981
Pat Cody <patcody028@gmail.com>
2020-01-20 13:03:14 -0500

    More PR comments

--- create_tables.sql
+++ create_tables.sql
@@ -1,7 +1,10 @@
-CREATE EXTENSION IF NOT EXISTS "pg-crypto";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE TYPE STATUS_COLOR AS ENUM ('GREEN', 'ORANGE', 'RED', 'GREY');
-CREATE TYPE DEPLOYMENT_STATUS AS ENUM ('ready-to-deploy', 'not-ready-to-deploy');
+DROP TYPE IF EXISTS STATUS_COLOR;
+CREATE TYPE STATUS_COLOR AS ENUM ('green', 'orange', 'red', 'grey');
+
+DROP TYPE IF EXISTS DEPLOYMENT_STATUS;
+CREATE TYPE DEPLOYMENT_STATUS AS ENUM ('ready', 'paused');
 
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
@@ -15,7 +18,7 @@ CREATE TABLE users (
 DROP TABLE IF EXISTS components CASCADE;
 CREATE TABLE components (
   component_id UUID DEFAULT gen_random_uuid(),
-  user_id SERIAL REFERENCES users(user_id),
+  user_id UUID REFERENCES users(user_id),
   github_repo TEXT NOT NULL,
   deployment_status DEPLOYMENT_STATUS NOT NULL,
   PRIMARY KEY(component_id)
@@ -31,9 +34,9 @@ CREATE TABLE workers (
 DROP TABLE IF EXISTS stats CASCADE;
 CREATE TABLE stats (
   stat_id UUID DEFAULT gen_random_uuid(),
-  worker_id SERIAL REFERENCES workers(worker_id),
-  component_id SERIAL REFERENCES components(component_id),
-  received_time TIMESTAMP NOT NULL,
+  worker_id UUID REFERENCES workers(worker_id),
+  component_id UUID REFERENCES components(component_id),
+  received_time TIMESTAMPTZ NOT NULL,
   color STATUS_COLOR NOT NULL,
   stat_window_seconds DOUBLE PRECISION NOT NULL,
   hits DOUBLE PRECISION NOT NULL,
@@ -46,11 +49,11 @@ CREATE TABLE stats (
 DROP TABLE IF EXISTS logs CASCADE;
 CREATE TABLE logs (
   log_id UUID DEFAULT gen_random_uuid(),
-  worker_id SERIAL REFERENCES workers(worker_id),
-  component_id SERIAL REFERENCES components(component_id),
+  worker_id UUID REFERENCES workers(worker_id),
+  component_id UUID REFERENCES components(component_id),
   execution_num INT NOT NULL,
-  received_time TIMESTAMP NOT NULL,
+  received_time TIMESTAMPTZ NOT NULL,
   log_text TEXT,
-  log_error VASRCHAR(400),
+  log_error TEXT,
   PRIMARY KEY (log_id)
 );
