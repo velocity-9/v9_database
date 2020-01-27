93d5dd9cda88212449c36d8f48343c1a7aa48404
Others <gregorpeach@gmail.com>
2020-01-27 00:06:51 -0500

    Add DEPLOYMENT_REASON to deploying table, make user email optional (#3)
    * Add DEPLOYMENT_REASON to deploying table, make user email optional
    
    * Make `worker_name` unique, since we can control that
    
    * GRANT permissions to v9_sys

--- create_tables.sql
+++ create_tables.sql
@@ -2,24 +2,32 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS components CASCADE;
+DROP TABLE IF EXISTS deploying CASCADE;
 DROP TABLE IF EXISTS workers CASCADE;
 DROP TABLE IF EXISTS stats CASCADE;
 DROP TABLE IF EXISTS logs CASCADE;
 
 DROP TYPE IF EXISTS STATUS_COLOR;
-DROP TYPE IF EXISTS DEPLOYMENT_INTENTION;
-
 CREATE TYPE STATUS_COLOR AS ENUM ('green', 'orange', 'red', 'grey');
+
+DROP TYPE IF EXISTS DEPLOYMENT_INTENTION;
 CREATE TYPE DEPLOYMENT_INTENTION AS ENUM ('active', 'paused');
 
-CREATE TABLE users (
+DROP TYPE IF EXISTS DEPLOYMENT_REASON;
+CREATE TYPE DEPLOYMENT_REASON AS ENUM ('initial_deployment', 'scaling');
+
+CREATE TABLE users
+(
   user_id UUID DEFAULT gen_random_uuid(),
-  email TEXT NOT NULL,
   github_username TEXT NOT NULL,
+  -- user email is optional
+  email TEXT DEFAULT NULL,
+  UNIQUE (github_username),
   PRIMARY KEY (user_id)
 );
 
-CREATE TABLE components (
+CREATE TABLE components
+(
   component_id UUID DEFAULT gen_random_uuid(),
   user_id UUID REFERENCES users(user_id),
   github_repo TEXT NOT NULL,
@@ -28,19 +36,25 @@ CREATE TABLE components (
   PRIMARY KEY(component_id)
 );
 
-CREATE TABLE deploying (
+CREATE TABLE deploying
+(
     component_id UUID REFERENCES components(component_id),
-    received_time TIMESTAMPTZ NOT NULL DEFAULT now(),
-    PRIMARY KEY(component_id, received_time)
+    deployment_start_time TIMESTAMPTZ NOT NULL DEFAULT now(),
+    deployment_reason DEPLOYMENT_REASON NOT NULL,
+    PRIMARY KEY(component_id)
 );
 
-CREATE TABLE workers (
+CREATE TABLE workers
+(
   worker_id UUID DEFAULT gen_random_uuid(),
   worker_name TEXT NOT NULL,
+  -- We control the `worker_name`, so it can be unique
+  UNIQUE(worker_name),
   PRIMARY KEY(worker_id)
 );
 
-CREATE TABLE stats (
+CREATE TABLE stats
+(
   stat_id UUID DEFAULT gen_random_uuid(),
   worker_id UUID REFERENCES workers(worker_id),
   component_id UUID REFERENCES components(component_id),
@@ -54,7 +68,8 @@ CREATE TABLE stats (
   PRIMARY KEY (stat_id)
 );
 
-CREATE TABLE logs (
+CREATE TABLE logs
+(
   log_id UUID DEFAULT gen_random_uuid(),
   worker_id UUID REFERENCES workers(worker_id),
   component_id UUID REFERENCES components(component_id),
@@ -64,3 +79,5 @@ CREATE TABLE logs (
   log_error TEXT,
   PRIMARY KEY (log_id)
 );
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO v9_sys;
